9fd6563288befd6fd47e1f43c8a84b6d86a77c3f
Yodahe Teshome <jodahe2001@gmail.com>
2024-06-17 18:57:55 +0300

    All Done Final Push

--- micard/lib/main.dart
+++ micard/lib/main.dart
@@ -12,31 +12,79 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("My Card"),
-          backgroundColor: const Color.fromARGB(255, 124, 241, 229),
+          title: const Center( child:  Text("My Card")),
+          backgroundColor: Color.fromARGB(255, 3, 88, 79),
         ),
-        body:const  Center(
-          child:  Column(
+        body:  const Center(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-         
             children: <Widget>[
-          CircleAvatar(
-            radius: 120.5,
-            backgroundImage: AssetImage('Assets/Image/OIG.lR.jpg'),
-          ),
-          Text(
-            "Jo DA",
-            style: TextStyle(
-              fontSize: 93.5,
-              fontWeight: FontWeight.bold,
-              color: Color.fromARGB(31, 181, 16, 16)
-            ),
-          )
+               CircleAvatar(
+                radius: 120.5,
+                backgroundImage: AssetImage('Assets/Image/OIG.lR.jpg'),
+              ),
+               Text(
+                "Jo DA",
+                style: TextStyle(
+                    fontSize: 53.5,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'PlaywriteSK-VariableFont_wght',
+                    color: Color.fromARGB(31, 10, 213, 166)
+                    ),
+              ),
+              Text('Flutter Developer',
+              style: TextStyle(
+                    fontSize: 20.5,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(240, 11, 209, 231)
+              ),
+              ),
+
+
+            Card(
+                color: Colors.amber,
+                margin:  EdgeInsets.symmetric(vertical: 5 , horizontal: 20),
+            child:  ListTile(
+              leading: Icon( Icons.phone),
+                title: Text('+251945897825',
+                       style: TextStyle(
+                    fontSize: 20.5,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 19, 20, 19)
+                    )
+                    ),
+            )
+
+              ),
+            
+              Card(
+                color: Colors.amber,
+                margin:  EdgeInsets.symmetric(vertical: 5 , horizontal: 20),
+            child:  ListTile(
+              leading: Icon( Icons.email),
+                title: Text('Jodahe@flutter.com',
+                       style: TextStyle(
+                    fontSize: 20.5,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 19, 20, 19)
+                    )
+                    ),
+            )
+
+              )
+
+
             ],
-            ),
+
+          ),
+
         ),
-          backgroundColor: const Color.fromARGB(255, 139, 120, 47),
+
+
+        backgroundColor: const Color.fromARGB(255, 4, 37, 56),
       ),
     );
   }
 }
+
+
