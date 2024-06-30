6a8259df93b991712d67597e7df8719cae1ed18c
Yodahe Teshome <jodahe2001@gmail.com>
2024-06-30 19:49:46 +0300

    Button updated to show signs of check(correct) and close (X) '

--- footballquiz/lib/main.dart
+++ footballquiz/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp( const Footballquiz());
+  runApp( Footballquiz());
 }
 
 class Footballquiz extends StatefulWidget {
@@ -13,96 +13,111 @@ class Footballquiz extends StatefulWidget {
 }
 
 class _FootballquizState extends State<Footballquiz> {
+  List<Icon> scoreKeeper = [
+       Icon(
+        Icons.check_box,
+        color: Color.fromARGB(255, 7, 234, 45),
+        size: 34.0,
+      ),
+       Icon(
+        Icons.close,
+        color: Colors.pink,
+        size: 34.0,
+      )
+    ];
   @override
   Widget build(BuildContext context) {
+     
+
     return MaterialApp(
       home: Scaffold(
-
-         backgroundColor: Colors.grey.shade900,
-         body:  SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
- children: [
-        
-        const Expanded(
-          flex: 5,
-          child: Padding(
-              padding:  EdgeInsets.all(10.0),
-            child: Center(
-              child: Text("Here is my Question" , 
-              textAlign: TextAlign.center,
-              style:  TextStyle(
-                        fontSize: 25.0,
-                        color: Colors.white,)
-                        ),
-            ),
-          ),
-        ),
-
-Expanded(
-          child: Padding(
-            padding:const EdgeInsets.all(15.0),
-            child: TextButton
-            (
-              style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(0, 12, 214, 72)),
-          ),
-             
-              child: const Text(
-                'True',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+              const Expanded(
+                flex: 5,
+                child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Center(
+                    child: Text("Here is my Question",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 25.0,
+                          color: Colors.white,
+                        )),
+                  ),
                 ),
               ),
-              onPressed: () {
-                //The user picked true.
-               
-              },
-            ),
-          ),
-        ),
-
-         Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: TextButton(
-              style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(0, 227, 56, 8)),
-          ),
-              child: const Text(
-                'False',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: TextButton(
+                    onPressed: () {
+  setState(() {
+    scoreKeeper.add(
+      Icon(
+        Icons.check_box,
+        color: Color.fromARGB(255, 7, 234, 45),
+        size: 34.0,
+      ),
+    );
+  }
+  );
+},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          const Color.fromARGB(0, 12, 214, 72)),
+                    ),
+                    child: const Text(
+                      'True',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                    
+                  )
                 ),
               ),
-              onPressed: () {
-                //The user picked false.
-                
-              },
-            ),
-          ),
-        ),
-
-        const Row(
-          children: [
-            Icon(Icons.check_box,
-            color: Color.fromARGB(255, 7, 234, 45),
-      size: 34.0,
-            ),
-            Icon(Icons.close,
-            color: Colors.pink,
-      size: 34.0,)
-          ],
-        )
-
- ]
-
-          )
-         
-         ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: TextButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          const Color.fromARGB(0, 227, 56, 8)),
+                    ),
+                    child: const Text(
+                      'False',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                           onPressed: () {
+  setState(() {
+    scoreKeeper.add(
+          Icon(
+        Icons.close,
+        color: Colors.pink,
+        size: 34.0,
+      )
+    );
+  }
+  );
+},
+                  ),
+                ),
+              ),
+               Expanded(
+                 child: Row(
+                  children: scoreKeeper,
+                               ),
+               )
+            ])),
       ),
     );
   }
